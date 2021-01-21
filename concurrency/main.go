9a5925edc1b2640cae6e575a6c5c947aa5a7a58a
TuanHoang <hoang@wolffungame.com>
2021-01-21 19:16:44 +0700

    go concurrency simple

---
+++ concurrency/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	//demoGo()
+	//demoChan()
+	doExercise()
+}
+
+func crawlData(note int, c chan int)  {
+	for  {
+		var timeDelay = myRand.Intn(MAX_DELAY - MIN_DELAY) + MIN_DELAY
+
+		time.Sleep(time.Duration(timeDelay) * time.Millisecond)
+
+		//fmt.Println("crawlData ", note)
+		c <- note
+	}
+
+}
+
+func showData(c chan int)  {
+	for i := 0; i < MAX_CHAN; i++ {
+		fmt.Println("Get value ", i, "data =", <-c)
+	}
+}
+
+var myRand *rand.Rand
+
+const MIN_DELAY = 100
+const MAX_DELAY = 200
+
+const MAX_CHAN = 1000
+const COUNT_CHAN_ONCE_TIME = 5
+
+func doExercise()  {
+	c := make(chan int, 1000)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	myRand = rand.New(s1)
+
+	for i := 0; i < COUNT_CHAN_ONCE_TIME; i++ {
+		x := i
+		go crawlData(x, c)
+	}
+	go showData(c)
+	time.Sleep(50 * time.Second)
+}
+
+func sum(note string, s []int, c chan int)  {
+	sum := 0
+	for _,v := range s{
+		sum += v
+	}
+	fmt.Println("Before = ", note)
+	c <- sum //bo sum vao cho chan
+	fmt.Println("Note = ", note)
+}
+
+func demoChan() {
+	s := []int {1, 2, 3, 4, 5, 6, 7, 8}
+
+	c := make(chan int, 2) //tao chan kieu int
+	go sum("lan1", s[ : len(s) / 2], c)
+
+	go sum("lan2", s[len(s)/2:], c)
+
+	x := <-c //lay gia tri tu c ra
+	fmt.Println("x = ", x)
+
+
+	go sum("lan3", s, c)
+	y := <-c //lay gia tri lan 2
+
+	fmt.Println("y = ", y)
+	fmt.Println( <-c)
+
+	go sum("lan4", s, c)
+	fmt.Println( <-c)
+}
+
+func demoGo()  {
+	go say("world")
+	say("hello")
+}
+
+func say(s string)  {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
